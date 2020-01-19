e98acab88c9238149e6b102f9f843c09d9da2eea
phuslu <phus.lu@gmail.com>
2020-01-19 13:41:24 +0000

    add files

---
+++ geoip_test.go
@@ -0,0 +1,74 @@
+package geoip
+
+import (
+	"net"
+	"testing"
+)
+
+func TestGeoIpCountry(t *testing.T) {
+	cases := []struct {
+		IP      string
+		Country string
+	}{
+		{"", ""},
+		{"0.0.0.0", "ZZ"},
+		{"0.1.1.1", "ZZ"},
+		{"1.1.1.1", "US"},
+		{"121.229.143.64", "CN"},
+		{"122.96.43.186", "CN"},
+		{"123.249.20.198", "CN"},
+		{"153.3.123.160", "CN"},
+		{"153.3.131.201", "CN"},
+		{"180.109.81.198", "CN"},
+		{"180.111.103.88", "CN"},
+		{"183.206.11.225", "CN"},
+		{"192.210.171.249", "US"},
+		{"223.112.9.2", "CN"},
+		{"23.16.28.232", "CA"},
+		{"58.240.115.210", "CN"},
+		{"61.155.4.66", "CN"},
+		{"255.255.255.255", "ZZ"},
+		{"2001:41d0:701:1100::29c8", "FR"},
+		{"2001:4860:4860::8888", "US"},
+		{"2001::6ca0:a535", "ZZ"},
+		{"2001:dc7:1000::1", "CN"},
+		{"2400:3200::1", "CN"},
+		{"2400:da00::6666", "CN"},
+		{"2404:6800:4008:801::2004", "TW"},
+		{"2404:6800:4012:1::200e", "AU"},
+		{"240C::6666", "CN"},
+		{"240e:4c:4008::1", "CN"},
+		{"240e:e8:f089:4877:70d2:775c:91d1:ab12", "CN"},
+		{"2620:0:2d0:200::7", "US"},
+		{"2a04:4e42:600::223", "NL"},
+		{"::", "ZZ"},
+		{"::1", "ZZ"},
+	}
+
+	for _, c := range cases {
+		country := Country(net.ParseIP(c.IP))
+		if string(country) != c.Country {
+			t.Errorf("Country(%#v) return \"%s\", expect %#v", c.IP, country, c.Country)
+		}
+	}
+}
+
+func BenchmarkGeoIpCountryForIPv4(b *testing.B) {
+	ip := net.ParseIP("8.8.8.8")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Country(net.IP(ip))
+	}
+}
+
+func BenchmarkGeoIpCountryForIPv6(b *testing.B) {
+	ip := net.ParseIP("2001:4860:4860::8888")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Country(ip)
+	}
+}
---
+++ update-db.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# pylint: disable=too-many-statements, line-too-long, W0703
+
+import io
+import urllib.request
+import struct
+import base64
+import zipfile
+import zlib
+
+
+def get(url: str) -> (list, list):
+    """extract country list and ip list from ip2loc url"""
+    zfile = zipfile.ZipFile(io.BytesIO(urllib.request.urlopen(url).read()))
+    text = zfile.read(next(x.filename for x in zfile.filelist if x.filename.endswith('.CSV')))
+    geo, ips = '', []
+    for line in io.BytesIO(text):
+        parts = line.strip().decode().split(',')
+        ip = parts[0].strip('"')
+        country = parts[2].strip('"')
+        if country == '-':
+            country = 'ZZ'
+        geo += country
+        ips.append(ip)
+    return geo, ips
+
+
+def gen6() -> str:
+    """generate ipv4 to geoip_db.go"""
+    geo, ips = get('http://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.IPV6.CSV.ZIP')
+    pack = lambda ip: struct.pack('<Q', ip >> 64) + struct.pack('<Q', ip & 0xFFFFFFFFFFFFFFFF)
+    ips = base64.b64encode(zlib.compress(b''.join(pack(int(x)) for x in ips))).decode()
+    return '''package geoip
+
+import (
+    "compress/zlib"
+    "encoding/base64"
+    "io/ioutil"
+    "strings"
+)
+
+var geo6 = []byte("%s")
+var ips6s = "%s"
+var ips6 = func() []byte {
+    r, _ := zlib.NewReader(base64.NewDecoder(base64.StdEncoding, strings.NewReader(ips6s)))
+    b, _ := ioutil.ReadAll(r)
+    return b
+}()
+''' % (geo, ips)
+
+
+def gen4() -> str:
+    """generate ipv4 to geoip_db.go"""
+    geo, ips = get('http://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.CSV.ZIP')
+    return '''
+var geo = []byte("%s")
+var ips = []uint32{%s}
+''' % (geo, ', '.join(ips))
+
+
+def main():
+    """convert ip2location country csv to geoip_db.go"""
+    text = gen6() + gen4()
+    with open('geoip_db.go', 'wb') as file:
+        file.write(text.encode())
+
+
+if __name__ == '__main__':
+    main()
+
