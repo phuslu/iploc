0782767a100715d97540a7b3291c8a843aca4226
phuslu <phus.lu@gmail.com>
2021-09-05 23:54:51 +0800

    improve performance

--- update-db.py
+++ update-db.py
@@ -12,33 +12,40 @@ def get(url: str) -> (list, list):
     """extract country list and ip list from ip2loc url"""
     zfile = zipfile.ZipFile(io.BytesIO(urllib.request.urlopen(url).read()))
     text = zfile.read(next(x.filename for x in zfile.filelist if x.filename.endswith('.CSV')))
-    ipinfo = []
+    iplist, geo = [], b''
     for line in io.BytesIO(text):
         parts = line.strip().decode().split(',')
         ip = parts[0].strip('"')
         country = parts[2].strip('"')
         if country == '-':
             country = 'ZZ'
-        ipinfo.append((ip, country.encode()))
-    return ipinfo
+        iplist.append(ip)
+        geo += country.encode()
+    return iplist, geo
 
 
 def gen_ip4_data() -> bytes:
     """generate ipv4 to binary data"""
-    ipinfo = get('http://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.CSV.ZIP')
-    pack = lambda ip: struct.pack('<I', ip)
-    data = b''.join(pack(int(ip))+country for (ip, country) in ipinfo)
-    with open('ipv4', 'wb') as file:
-        file.write(data)
+    iplist, geo = get('http://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.CSV.ZIP')
+    pack = lambda e, ip: struct.pack(e+'I', ip)
+    with open('ipv4.txt', 'wb') as file:
+        file.write(geo)
+    with open('ipv4le.bin', 'wb') as file:
+        file.write(b''.join(pack('<', int(ip)) for ip in iplist))
+    with open('ipv4be.bin', 'wb') as file:
+        file.write(b''.join(pack('>', int(ip)) for ip in iplist))
 
 
 def gen_ip6_data() -> bytes:
     """generate ipv6 to binary data"""
-    ipinfo = get('http://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.IPV6.CSV.ZIP')
-    pack = lambda ip: struct.pack('<Q', ip >> 64) + struct.pack('<Q', ip & 0xFFFFFFFFFFFFFFFF)
-    data = b''.join(pack(int(ip))+country for (ip, country) in ipinfo)
-    with gzip.GzipFile('ipv6.gz', 'wb') as file:
-        file.write(data)
+    iplist, geo = get('http://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.IPV6.CSV.ZIP')
+    pack = lambda e, ip: struct.pack(e+'Q', ip >> 64) + struct.pack(e+'Q', ip & 0xFFFFFFFFFFFFFFFF)
+    with open('ipv6.txt', 'wb') as file:
+        file.write(geo)
+    with gzip.GzipFile('ipv6le.gz', 'wb') as file:
+        file.write(b''.join(pack('<', int(ip)) for ip in iplist))
+    with gzip.GzipFile('ipv6be.gz', 'wb') as file:
+        file.write(b''.join(pack('>', int(ip)) for ip in iplist))
 
 
 def main():
