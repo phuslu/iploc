990d65545936b54d776e6bd5cbd9ec6c576e5af5
phuslu <phus.lu@gmail.com>
2021-09-05 23:54:51 +0800

    improve performance
    Former-commit-id: 0782767a100715d97540a7b3291c8a843aca4226

--- iploc.go
+++ iploc.go
@@ -18,17 +18,42 @@ package iploc
 import (
 	"bytes"
 	"compress/gzip"
+	_ "embed" // for ip data
 	"encoding/binary"
 	"io"
 	"net"
+	"reflect"
+	"unsafe"
 )
 
 // Version is iplocation database version.
 const Version = "v1.0.20210830"
 
+var ip4uint []uint32
+var ip6uint []uint64
+
+//go:embed ipv4.txt
+var ip4txt []byte
+
+//go:embed ipv6.txt
+var ip6txt []byte
+
 func init() {
-	r, _ := gzip.NewReader(bytes.NewReader(ip6data))
-	ip6data, _ = io.ReadAll(r)
+	// ipv4
+	ip4uint = *(*[]uint32)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(&ip4bin[0])),
+		Len:  len(ip4bin) / 4,
+		Cap:  len(ip4bin) / 4,
+	}))
+
+	// ipv6
+	r, _ := gzip.NewReader(bytes.NewReader(ip6bin))
+	ip6bin, _ = io.ReadAll(r)
+	ip6uint = *(*[]uint64)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(&ip6bin[0])),
+		Len:  len(ip6bin) / 16,
+		Cap:  len(ip6bin) / 16,
+	}))
 }
 
 // Country return ISO 3166-1 alpha-2 country code of IP.
@@ -40,32 +65,33 @@ func Country(ip net.IP) (country []byte) {
 	if ip4 := ip.To4(); ip4 != nil {
 		// ipv4
 		n := binary.BigEndian.Uint32(ip4)
-		i, j := 0, len(ip4data)/6
+		i, j := 0, len(ip4uint)
+		_ = ip4uint[j-1]
 		for i < j {
 			h := (i + j) >> 1
-			if ip4uint(h) > n {
+			if ip4uint[h] > n {
 				j = h
 			} else {
 				i = h + 1
 			}
 		}
-		country = ip4data[i*6-2 : i*6]
+		country = ip4txt[i*2-2 : i*2]
 	} else {
-
 		// ipv6
 		high := binary.BigEndian.Uint64(ip)
 		low := binary.BigEndian.Uint64(ip[8:])
-		i, j := 0, len(ip6data)/18
+		i, j := 0, len(ip6uint)
+		_ = ip6uint[j-1]
 		for i < j {
-			h := (i + j) >> 1
-			n := ip6uint(h)
-			if n > high || (n == high && ip6uint(h+1) > low) {
+			h := (i + j) >> 1 & ^1
+			n := ip6uint[h]
+			if n > high || (n == high && ip6uint[h+1] > low) {
 				j = h
 			} else {
-				i = h + 1
+				i = h + 2
 			}
 		}
-		country = ip6data[i*18-2 : i*18]
+		country = ip6txt[i-2 : i]
 	}
 
 	return
---
+++ iploc_le.go
@@ -0,0 +1,13 @@
+// +build 386 amd64 amd64p32 arm64 ppc64le mipsle mips64le mips64p32le
+
+package iploc
+
+import (
+	_ "embed" // for ip data
+)
+
+//go:embed ipv4le.bin
+var ip4bin []byte
+
+//go:embed ipv6le.gz
+var ip6bin []byte
