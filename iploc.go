edba835f5424dbff713978db1b40f30fdb946017
phuslu <phus.lu@gmail.com>
2024-03-11 14:06:22 +0800

    remove bounds checking
    Former-commit-id: a3a6e33477af863e9aa913b1b80e19ab606de308
    Former-commit-id: de59e81cad358423ecc0791105a5b4b35ac84258

--- iploc.go
+++ iploc.go
@@ -24,15 +24,11 @@ import (
 	"net"
 	"net/netip"
 	"os"
-	"reflect"
 	"unsafe"
 )
 
 // Version is iplocation database version.
 const Version = "v1.0.20240229"
-
-var ip4uint []uint32
-var ip6uint []uint64
 
 //go:embed ipv4.txt
 var ip4txt []byte
@@ -41,24 +37,10 @@ var ip4txt []byte
 var ip6txt []byte
 
 func init() {
-	// ipv4
-	ip4uint = *(*[]uint32)(unsafe.Pointer(&reflect.SliceHeader{
-		Data: uintptr(unsafe.Pointer(&ip4bin[0])),
-		Len:  len(ip4bin) / 4,
-		Cap:  len(ip4bin) / 4,
-	}))
-
 	// ipv6
 	if os.Getenv("IPLOC_IPV4ONLY") == "" {
 		r, _ := gzip.NewReader(bytes.NewReader(ip6bin))
 		ip6bin, _ = io.ReadAll(r)
-		ip6uint = *(*[]uint64)(unsafe.Pointer(&reflect.SliceHeader{
-			Data: uintptr(unsafe.Pointer(&ip6bin[0])),
-			Len:  len(ip6bin) / 8,
-			Cap:  len(ip6bin) / 8,
-		}))
-	} else {
-		ip6uint = []uint64{0, 0}
 	}
 }
 
@@ -71,11 +53,10 @@ func Country(ip net.IP) (country []byte) {
 	if ip4 := ip.To4(); ip4 != nil {
 		// ipv4
 		n := binary.BigEndian.Uint32(ip4)
-		i, j := 0, len(ip4uint)
-		_ = ip4uint[j-1]
+		i, j := 0, len(ip4bin)/4
 		for i < j {
 			h := (i + j) >> 1
-			if ip4uint[h] > n {
+			if *(*uint32)(unsafe.Add(unsafe.Pointer(&ip4bin[0]), uintptr(h*4))) > n {
 				j = h
 			} else {
 				i = h + 1
@@ -86,12 +67,11 @@ func Country(ip net.IP) (country []byte) {
 		// ipv6
 		high := binary.BigEndian.Uint64(ip)
 		low := binary.BigEndian.Uint64(ip[8:])
-		i, j := 0, len(ip6uint)
-		_ = ip6uint[j-1]
+		i, j := 0, len(ip6bin)/8
 		for i < j {
 			h := (i + j) >> 1 & ^1
-			n := ip6uint[h]
-			if n > high || (n == high && ip6uint[h+1] > low) {
+			n := *(*uint64)(unsafe.Add(unsafe.Pointer(&ip6bin[0]), uintptr(h*8)))
+			if n > high || (n == high && *(*uint64)(unsafe.Add(unsafe.Pointer(&ip6bin[0]), uintptr((h+1)*8))) > low) {
 				j = h
 			} else {
 				i = h + 2
@@ -109,11 +89,10 @@ func IPCountry(ip netip.Addr) (country []byte) {
 		// ipv4
 		b := ip.As4()
 		n := uint32(b[0])<<24 | uint32(b[1])<<16 | uint32(b[2])<<8 | uint32(b[3])
-		i, j := 0, len(ip4uint)
-		_ = ip4uint[j-1]
+		i, j := 0, len(ip4bin)/4
 		for i < j {
 			h := (i + j) >> 1
-			if ip4uint[h] > n {
+			if *(*uint32)(unsafe.Add(unsafe.Pointer(&ip4bin[0]), uintptr(h*4))) > n {
 				j = h
 			} else {
 				i = h + 1
@@ -125,12 +104,11 @@ func IPCountry(ip netip.Addr) (country []byte) {
 		b := ip.As16()
 		high := uint64(b[0])<<56 | uint64(b[1])<<48 | uint64(b[2])<<40 | uint64(b[3])<<32 | uint64(b[4])<<24 | uint64(b[5])<<16 | uint64(b[6])<<8 | uint64(b[7])
 		low := uint64(b[8])<<56 | uint64(b[9])<<48 | uint64(b[10])<<40 | uint64(b[11])<<32 | uint64(b[12])<<24 | uint64(b[13])<<16 | uint64(b[14])<<8 | uint64(b[15])
-		i, j := 0, len(ip6uint)
-		_ = ip6uint[j-1]
+		i, j := 0, len(ip6bin)/8
 		for i < j {
 			h := (i + j) >> 1 & ^1
-			n := ip6uint[h]
-			if n > high || (n == high && ip6uint[h+1] > low) {
+			n := *(*uint64)(unsafe.Add(unsafe.Pointer(&ip6bin[0]), uintptr(h*8)))
+			if n > high || (n == high && *(*uint64)(unsafe.Add(unsafe.Pointer(&ip6bin[0]), uintptr((h+1)*8))) > low) {
 				j = h
 			} else {
 				i = h + 2
